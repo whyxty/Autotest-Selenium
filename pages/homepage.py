fed6984a00825f0717d827fa89f3c7b93a5f41cf
Arsen <arsenzaripoa3@gmail.com>
2025-04-20 17:59:22 +0500

    1.5 (STABLE)

--- pages/homepage.py
+++ pages/homepage.py
@@ -1,4 +1,6 @@
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
 class HomePage:
@@ -10,7 +12,14 @@ class HomePage:
         self.driver.get('https://www.demoblaze.com/index.html')
 
     def click_galaxy_s6(self):
-        galaxy_s6 = self.driver.find_element(By.LINK_TEXT, 'Samsung galaxy s6').click()
+        try:
+            wait = WebDriverWait(self.driver, 15)
+            element = wait.until(EC.element_to_be_clickable((By.LINK_TEXT, "Samsung galaxy s6")))
+            element.click()
+        except Exception as e:
+            self.driver.save_screenshot("error_galaxy_s6.png")
+            raise e
+
 
     def click_monitors(self):
         monitors = self.driver.find_element(By.XPATH, '/html/body/div[5]/div/div[1]/div/a[4]').click()
--- pages/product.py
+++ pages/product.py
@@ -1,11 +1,19 @@
 from selenium.webdriver.common.by import By
-
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 class ProductPage:
-
     def __init__(self, driver):
         self.driver = driver
+        self.wait = WebDriverWait(self.driver, 15)  # Добавляем инициализацию wait
 
-    def check_title_is(self, title):
-        page_title = self.driver.find_element(By.XPATH, '//*[@id="tbodyid"]/h2')
-        assert page_title.text == 'Samsung galaxy s6'
+    def check_title_is(self, expected_title):
+        try:
+            # Сначала ждем появления элемента, потом проверяем текст
+            title_element = self.wait.until(
+                EC.visibility_of_element_located((By.XPATH, '//h2[@class="name"]'))
+            )
+            assert expected_title.lower() in title_element.text.lower()
+        except Exception as e:
+            self.driver.save_screenshot("product_title_error.png")
+            raise e
