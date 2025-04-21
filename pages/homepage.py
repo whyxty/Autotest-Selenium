d75872c5081e147a0fd28e4182b90d22d82dec3c
Arsen <arsenzaripoa3@gmail.com>
2025-04-22 01:12:27 +0500

    1.7 (UNSTABLE)

--- pages/homepage.py
+++ pages/homepage.py
@@ -1,8 +1,7 @@
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import random
-import string
+
 
 class HomePage:
 
@@ -27,41 +26,4 @@ class HomePage:
     def check_products_count(self, count):
         monitors = self.driver.find_elements(By.CLASS_NAME, 'card-title')
         assert len(monitors) == count
-
-    def click_sign_up(self):
-        try:
-            wait = WebDriverWait(self.driver, 15)
-            element = wait.until(EC.element_to_be_clickable((By.CLASS_NAME, "nav-link")))
-            element.click()
-        except Exception as e:
-            self.driver.save_screenshot("error_sign_up.png")
-            raise e
-
-    def generate_password(length):
-        characters = string.ascii_letters + string.digits + string.punctuation
-        return ''.join(random.sample(characters, length))
-
-    def generate_login(self):
-        characters = string.ascii_letters + string.digits + string.punctuation
-        return ''.join(random.sample(characters, self))
-
-    def click_login(self):
-        try:
-            wait = WebDriverWait(self.driver, 15)
-            element = wait.until(EC.element_to_be_clickable((By.ID, "sign-username")))
-            element.click()
-        except Exception as e:
-            self.driver.save_screenshot("error_login.png")
-            raise e
-
-    def click_password(self):
-        try:
-            wait = WebDriverWait(self.driver, 15)
-            element = wait.until(EC.element_to_be_clickable((By.ID, "sign-password")))
-            element.click()
-        except Exception as e:
-            self.driver.save_screenshot("error_password.png")
-            raise e
-
-
 
